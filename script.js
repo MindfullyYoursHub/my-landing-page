823abb87f635ee8f32d7bfb167e35e2b16f91813
MindfullyYoursHub <mindfullyyours2023@gmail.com>
2025-07-29 13:32:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+  const toggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('nav-links');
 
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+    toggle.classList.toggle('open');
+  });
